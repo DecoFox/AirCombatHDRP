f0f899d7724592e8e84d7291895f9c56021553a4
Balto-the-Wolf-Dog <ppachuta@gmail.com>
2021-05-25 16:08:53 -0800

    asdf

---
+++ Assets/ProceduralTexturer/ProceduralTexMaster.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralTexMaster
+{
+    [System.Serializable]
+    public class TileTexture
+    {
+        public string Name;
+        public Texture2D Texture;
+        public Texture2D Normal;
+        public float MinimumHeight;
+        public float MaximumHeight;
+        public float MinimumSteepness;
+        public float MaximumSteepness;
+        public float scale;
+        public float u;
+        public float v;
+
+        public float SteepWeight = 1;
+        public float HeightWeight = 1;
+    }
+
+    [System.Serializable]
+    public class LayerAlias //We can't work with actual TileTexture objects in threads, and we don't need all of their information anyway. This exists to store data from these objects for use in a thread according to a corresonding index.
+    {
+        public float MinimumHeight;
+        public float MaximumHeight;
+        public float MinimumSteepness;
+        public float MaximumSteepness;
+
+        public float SteepWeight;
+        public float HeightWeight;
+    }
+}
---
+++ Assets/ProceduralTexturer/ProceduralTexServer.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ProceduralTexMaster;
+
+public class ProceduralTexServer : MonoBehaviour
+{
+    public List<TileTexture> TileTextures = new List<TileTexture>();
+}
